b1ea07d70c6abfb46c53d9739bcc01bcd35e5fd4
Mahima Manik <mahimamanik.22@gmail.com>
2024-08-06 13:15:18 +0700

    Setup the CTA section

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import config from "@/config";
 
@@ -5,25 +6,41 @@ const CTA = () => {
   return (
     <section className="relative hero overflow-hidden min-h-screen">
       <Image
-        src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
+        src="/cta_cover.png"
         alt="Background"
         className="object-cover w-full"
         fill
       />
-      <div className="relative hero-overlay bg-neutral bg-opacity-70"></div>
+      <div className="relative hero-overlay bg-neutral bg-opacity-10"></div>
       <div className="relative hero-content text-center text-neutral-content p-8">
         <div className="flex flex-col items-center max-w-xl p-8 md:p-0">
-          <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 md:mb-12">
-            Boost your app, launch, earn
+          <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 md:mb-12 text-white">
+            Don&apos;t waste your time thinking. Take the first step today.
           </h2>
           <p className="text-lg opacity-80 mb-12 md:mb-16">
-            Don&apos;t waste time integrating APIs or designing a pricing
-            section...
+            
           </p>
 
-          <button className="btn btn-primary btn-wide">
-            Get {config.appName}
-          </button>
+          <div className="flex gap-4">
+          <a href="https://play.google.com/store/apps/details?id=your.app.id" target="_blank">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+              alt="Get it on Google Play"
+              style={{ width: "200px", height: "60px" }}
+              width="200"
+              height="60"
+            />
+          </a>
+          <a href="https://apps.apple.com/us/app/your-app-id" target="_blank">
+            <img
+              src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+              alt="Download on the App Store"
+              style={{ width: "200px", height: "60px" }}
+              width="200"
+              height="60"
+            />
+            </a>
+          </div>
         </div>
       </div>
     </section>
